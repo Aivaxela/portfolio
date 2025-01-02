8ac80f1cdbf86bcd54acc625e204a2270aa7adef
Riley Marcum <rmarcum13@gmail.com>
2025-01-01 22:00:16 -0500

    reorg and small style changes

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { Clusters } from "./Clusters";
+import { Header } from "./Header";
+import { Intro } from "./Intro";
+
+export function App() {
+  return (
+    <div className="flex flex-col items-center mx-auto max-w-[1440px] h-screen bg-gray-100">
+      <Header />
+      <Intro />
+      <Clusters />
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <header className="flex flex-col items-center bg-gradient-to-b from-teal-200 to-teal-100 p-5 w-full">
-      <h1 className="text-3xl font-bold">Riley Marcum</h1>
-      <h2 className="text-xl font-semibold">
+      <h1 className="text-[72px] font-bold">Riley Marcum</h1>
+      <h2 className="text-xl font-semibold text-gray-600">
         IoT Specialist | Software Engineer | Hobbyist Game Developer
       </h2>
     </header>
