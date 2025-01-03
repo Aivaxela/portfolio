e98de35c8288a9b58dc92ff07f7ed8b0600fd750
Riley Marcum <rmarcum13@gmail.com>
2025-01-03 02:56:27 -0500

    ne images imported, cluster styling updates

--- src/components/Cluster.jsx
+++ src/components/Cluster.jsx
@@ -1,10 +1,33 @@
-export function Cluster({ name, imgPrimary, imgSecondary, imgTertiary }) {
+export function Cluster({
+  name,
+  description,
+  imgPrimary,
+  imgSecondary,
+  imgTertiary,
+}) {
+  const commonImageAttributes =
+    "rounded-lg border-4 border-gray-700 shadow-2xl";
+
   return (
-    <div>
-      <h2>Project name</h2>
-      <img src="" alt="project image primary" />
-      <img src="" alt="project image secondary" />
-      <img src="" alt="project image secondary" />
+    <div className="w-[500px] h-[500px] flex flex-col items-center justify-center">
+      <div className="relative w-[300px] h-[300px]">
+        <img
+          src={imgPrimary}
+          alt="project image primary"
+          className={`absolute w-[300px] h-[300px] ${commonImageAttributes}`}
+        />
+        <img
+          src={imgSecondary}
+          alt="project image secondary"
+          className={`absolute z-10 w-[175px] h-[175px] left-[-65px] bottom-[20px] ${commonImageAttributes}`}
+        />
+        <img
+          src={imgTertiary}
+          alt="project image secondary"
+          className={`absolute z-20 w-[100px] h-[100px] right-[-45px] top-[40px] ${commonImageAttributes}`}
+        />
+      </div>
+      <h2 className="text-[30px] p-6 font-bold">{name}</h2>
     </div>
   );
 }
--- src/components/Clusters.jsx
+++ src/components/Clusters.jsx
@@ -1,5 +1,32 @@
 import { Cluster } from "./Cluster";
+import ne1 from "../assets/ne_1.png";
+import ne2 from "../assets/ne_2.png";
+import ne3 from "../assets/ne_3.png";
 
 export function Clusters() {
-  return <Cluster></Cluster>;
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <Cluster
+        name="NewsExplorer"
+        description="A project created with React and Vite that allows visitors to search for news articles and save them to their personal profile"
+        imgPrimary={ne1}
+        imgSecondary={ne2}
+        imgTertiary={ne3}
+      ></Cluster>
+      <Cluster
+        name="NewsExplorer"
+        description="A project created with React and Vite that allows visitors to search for news articles and save them to their personal profile"
+        imgPrimary={ne1}
+        imgSecondary={ne2}
+        imgTertiary={ne3}
+      ></Cluster>
+      <Cluster
+        name="NewsExplorer"
+        description="A project created with React and Vite that allows visitors to search for news articles and save them to their personal profile"
+        imgPrimary={ne1}
+        imgSecondary={ne2}
+        imgTertiary={ne3}
+      ></Cluster>
+    </div>
+  );
 }
