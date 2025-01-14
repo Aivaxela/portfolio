c67674a0beebe525a178a6728ef5c28f428929ff
Riley Marcum <rmarcum13@gmail.com>
2025-01-14 00:56:31 -0500

    add some responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import me from "../assets/me-removebg.png";
 export function Header() {
   return (
     <header className="flex flex-col items-center bg-gradient-to-b p-10 from-teal-200 to-teal-100 w-full">
-      <div className="flex justify-between w-full">
+      <div className="flex flex-col gap-3 md:flex-row md:gap-0 justify-between w-full">
         <div className="flex flex-col justify-center text-center ml-5">
-          <h1 className="text-[84px] font-bold">Riley Marcum</h1>
-          <h2 className="text-xl font-semibold text-gray-500">
+          <h1 className="text-[48px] md:text-[84px] font-bold">Riley Marcum</h1>
+          <h2 className="text-md md:text-xl font-semibold text-gray-500">
             IoT Specialist | Software Engineer | Hobbyist Game Developer |
             Continuous Learner
           </h2>
         </div>
         <img
-          className="w-[300px] h-[300px] rounded-full bg-teal-100 p-2 shadow-2xl"
+          className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] self-center rounded-full bg-teal-100 p-2 shadow-2xl"
           src={me}
           alt="portrait photo of Riley"
         />
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,7 @@
 export function Intro() {
   return (
     <section className="flex flex-col text-center p-5 my-5 w-10/12 bg-gradient-to-b from-blue-200 to-blue-100 rounded-3xl">
-      <p className="text-lg">
+      <p className="text-md md:text-lg">
         Hello, my name is Riley Marcum. Thank you for spending the time to take
         a look at my projects!
       </p>
