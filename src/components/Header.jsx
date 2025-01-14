0dc5433c3c7ccdd4ca4a9bee62a023c1f8680803
Riley Marcum <rmarcum13@gmail.com>
2025-01-14 00:34:01 -0500

    adjust script and update font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
       <div className="flex justify-between w-full">
         <div className="flex flex-col justify-center text-center ml-5">
           <h1 className="text-[84px] font-bold">Riley Marcum</h1>
-          <h2 className="text-xl font-semibold text-gray-600">
+          <h2 className="text-xl font-semibold text-gray-500">
             IoT Specialist | Software Engineer | Hobbyist Game Developer |
             Continuous Learner
           </h2>
